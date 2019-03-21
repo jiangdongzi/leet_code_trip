8b0104f637ffdb20a504928944d25ed9763b4156
liuzhenguo <liuzhenguo@xiaomi.com>
2019-03-21 18:04:30 +0800

    new file:   findMedianSortedArrays/Solution.java

---
+++ findMedianSortedArrays/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		if (nums1 == null || nums1.length == 0) {
+			if ((nums2.length & 1) == 0) {
+				return nums2[nums2.length / 2] / 2.0 + nums2[nums2.length / 2 - 1] / 2.0;
+			} else {
+				return nums2[nums2.length / 2];
+			}
+		}
+
+		if (nums2 == null || nums2.length == 0) {
+			if ((nums1.length & 1) == 0) {
+				return nums1[nums1.length / 2] / 2.0 + nums1[nums1.length / 2 - 1] / 2.0;
+			} else {
+				return nums1[nums1.length / 2];
+			}
+		}
+
+		if (nums1[nums1.length - 1] <= nums2[0]) {
+			if (nums1.length < nums2.length) {
+
+			}
+		}
+
+	}
+}
