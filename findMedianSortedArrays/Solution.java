7f81aa6dd90d9de2c32e5cea95dfef6babe637fc
liuzhenguo <liuzhenguo@xiaomi.com>
2019-03-22 15:52:17 +0800

    modified:   Solution.java

--- findMedianSortedArrays/Solution.java
+++ findMedianSortedArrays/Solution.java
@@ -1,4 +1,4 @@
-class Solution {
+ass Solution {
 	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 		if (nums1 == null || nums1.length == 0) {
 			if ((nums2.length & 1) == 0) {
@@ -18,9 +18,22 @@ class Solution {
 
 		if (nums1[nums1.length - 1] <= nums2[0]) {
 			if (nums1.length < nums2.length) {
-
+				if ((nums1.length + nums2.length) % 2 == 0) {
+					return nums2[(nums2.length - nums1.length) / 2] / 2.0 + nums2[(nums2.length - nums1.length) / 2 - 1] / 2.0;
+				} else {
+					return nums2[(nums2.length - nums1.length) / 2];
+				}
+			} else if (nums1.length == nums2.length) {
+				return nums1[nums1.length - 1] / 2.0 + nums2[0] / 2.0;
+			} else {
+				if ((nums1.length + nums2.length) % 2 == 0) {
+					return nums1[(nums1.length + nums2.length) / 2] / 2.0 + nums1[(nums1.length + nums2.length) / 2 - 1] / 2.0;
+				} else {
+					return nums1[(nums1.length + nums2.length) / 2];
+				}
 			}
 		}
 
+		return 1;
 	}
 }
